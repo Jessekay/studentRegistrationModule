82f5d2cac1ae6035357cb314c07a1a6f33106c50
Jessekay <kayjes00@gmail.com>
2025-10-12 20:50:42 +0200

    created a student model

---
+++ studentRegistration/src/main/java/com/example/studentRegistration/model/Student.java
@@ -0,0 +1,27 @@
+package com.example.studentRegistration.model;
+
+public class Student {
+    private String regNumber;
+    private String firstName;
+    private String dateOfBirth;
+    public String getRegNumber() {
+        return regNumber;
+    }
+    public void setRegNumber(String regNumber) {
+        this.regNumber = regNumber;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    
+}
