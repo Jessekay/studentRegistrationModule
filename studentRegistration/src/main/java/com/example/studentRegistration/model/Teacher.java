648c3af6a4b3f3f0c23dd44516fcea050fa444fd
Jessekay <kayjes00@gmail.com>
2025-10-12 22:06:08 +0200

    created Teacher model with the getters and setters method

---
+++ studentRegistration/src/main/java/com/example/studentRegistration/model/Teacher.java
@@ -0,0 +1,26 @@
+package com.example.studentRegistration.model;
+
+public class Teacher {
+    private String code;
+    private String names;
+    private String qualifications;
+    public String getCode() {
+        return code;
+    }
+    public void setCode(String code) {
+        this.code = code;
+    }
+    public String getNames() {
+        return names;
+    }
+    public void setNames(String names) {
+        this.names = names;
+    }
+    public String getQualifications() {
+        return qualifications;
+    }
+    public void setQualifications(String qualifications) {
+        this.qualifications = qualifications;
+    }
+
+}
