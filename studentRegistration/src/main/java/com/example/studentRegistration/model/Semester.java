1babb786e6dd5f6fb32ee69f57827570a6375186
Jessekay <kayjes00@gmail.com>
2025-10-12 22:12:53 +0200

    implemented a semester model class with the getters and setters methods

---
+++ studentRegistration/src/main/java/com/example/studentRegistration/model/Semester.java
@@ -0,0 +1,35 @@
+package com.example.studentRegistration.model;
+
+import java.time.LocalDate;
+
+public class Semester {
+    private String id;
+    private String name;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+    
+}
