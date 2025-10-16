1775a5d6a511eda25d7a889e5b1f6acfb7e564ce
Jessekay <kayjes00@gmail.com>
2025-10-16 08:42:19 +0200

    created address model class with getters and setters

---
+++ studentRegistration/src/main/java/com/example/studentRegistration/model/Address.java
@@ -0,0 +1,42 @@
+package com.example.studentRegistration.model;
+
+public class Address {
+    private String province;
+    private String district;
+    private String cell;
+    private String village;
+    public String getProvince() {
+        return province;
+    }
+    public void setProvince(String province) {
+        this.province = province;
+    }
+    public String getDistrict() {
+        return district;
+    }
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+    public String getCell() {
+        return cell;
+    }
+    public void setCell(String cell) {
+        this.cell = cell;
+    }
+    public String getVillage() {
+        return village;
+    }
+    public void setVillage(String village) {
+        this.village = village;
+    }
+    public Address() {
+    }
+    public Address(String province, String district, String cell, String village) {
+        this.province = province;
+        this.district = district;
+        this.cell = cell;
+        this.village = village;
+    }
+
+    
+}
